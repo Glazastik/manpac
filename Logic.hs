ceaa6d47dc4ee90c4eae7103baf691d28975f896
Viktor Karlsson <wiixtor@gmail.com>
2014-12-08 21:37:36 +0100

    Testing the waters

--- Logic.hs
+++ Logic.hs
@@ -1,2 +1,12 @@
 module Logic where
 import Haste.Graphics.Canvas
+
+-- | The state of our game.
+data GameState = GameState {
+    manPacPos :: Point
+  }
+
+-- Width and height of the playing field.
+width, height :: Double
+width = 800
+height = 500
--- Main.hs
+++ Main.hs
@@ -2,5 +2,14 @@ import Haste
 import Haste.Graphics.Canvas
 import Logic
 
+-- Create a new canvas to draw on.
+newCanvas :: Double -> Double -> IO Elem
+newCanvas w h = do
+	canvas <- newElem "canvas"
+	return canvas
 
-main = undefined
+main = do
+	canvasElem <- newCanvas width height
+	setProp canvas "width" (show w)
+    setProp canvas "height" (show h)
+	Just canvas <- getCanvas canvasElem
