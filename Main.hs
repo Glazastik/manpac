1db396337df43dd69099bda64b7472120d6d759b
Viktor Karlsson <wiixtor@gmail.com>
2014-12-08 21:45:11 +0100

    Some more testing

--- Main.hs
+++ Main.hs
@@ -6,10 +6,14 @@ import Logic
 newCanvas :: Double -> Double -> IO Elem
 newCanvas w h = do
 	canvas <- newElem "canvas"
+	setStyle canvas "border" "1px solid black"
+	setStyle canvas "display" "block"
+	setStyle canvas "margin" "auto"
+	setStyle canvas "background-color" "pink"
+	setProp canvas "width" (show w)
+	setProp canvas "height" (show h)
 	return canvas
 
 main = do
 	canvasElem <- newCanvas width height
-	setProp canvas "width" (show w)
-    setProp canvas "height" (show h)
 	Just canvas <- getCanvas canvasElem
