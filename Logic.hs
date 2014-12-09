4e7ceeac364bdcaac02779fa7c2d67de599d6a49
Wiixtor <wiixtor@gmail.com>
2014-12-09 23:45:38 +0100

    Made layout prettier

--- Logic.hs
+++ Logic.hs
@@ -65,8 +65,8 @@ initialState = GameState {
 }
 
 walls :: [Rect]
-walls = [Rect (manPacRadius*2 + p*(manPacRadius*3)) (manPacRadius*2) manPacRadius (manPacRadius*17) | p <- [0..25] ] 
-		++ [Rect (manPacRadius*2 + p*(manPacRadius*15)) (manPacRadius*21) (manPacRadius*13) manPacRadius | p <- [0..4] ]
+walls = [Rect (manPacRadius*2 + p*(manPacRadius*15)) (manPacRadius*2) (manPacRadius*13) manPacRadius | p <- [0..4] ]
+		++ [Rect (manPacRadius*2 + p*(manPacRadius*3)) (manPacRadius*5) manPacRadius (manPacRadius*17) | p <- [0..25] ] 
 		++ [Rect (manPacRadius*2 + p*(manPacRadius*15)) (manPacRadius*24) (manPacRadius*13) manPacRadius | p <- [0..4] ]
 		++ [Rect (manPacRadius*2 + p*(manPacRadius*3)) (manPacRadius*27) manPacRadius (manPacRadius*17) | p <- [0..25] ] 
 		++ [Rect (manPacRadius*2 + p*(manPacRadius*15)) (manPacRadius*46) (manPacRadius*13) manPacRadius | p <- [0..4] ]
