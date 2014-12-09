d3049552aa285945ff8a33c8fbcaa06494d6837f
Wiixtor <wiixtor@gmail.com>
2014-12-09 19:55:40 +0100

    First draft for moveManPac

--- Logic.hs
+++ Logic.hs
@@ -1,4 +1,5 @@
 module Logic where
+import qualified Data.Set as S
 import Haste.Graphics.Canvas
 
 -- | The state of our game.
@@ -16,6 +17,10 @@ height = 500
 manPacRadius :: Double
 manPacRadius = 50
 
+-- How fast is manPac
+manPacSpeed :: Double
+manPacSpeed = 10
+
 -- | Move a point by a certain velocity.
 move :: Point -> Vector -> Point
 move (x, y) (xv, yv) = (x + xv, y + yv)
@@ -24,6 +29,21 @@ move (x, y) (xv, yv) = (x + xv, y + yv)
 clamp :: Point -> Rect -> Point
 clamp (x, y) (Rect xMin yMin xMax yMax) =
   (min (max x xMin) xMax, min (max y yMin) yMax)
+
+-- | Update the paddles depending on the currently pressed keys.
+moveManPac :: S.Set Char -> GameState -> GameState
+moveManPac keys state = state {
+      manPacPos  = movePac 'W' 'S' 'A' 'D' (manPacPos state) `clamp` playingField
+    }
+  where
+    playingField = Rect 0 0 width height
+    movePac up down left right pac
+      | up `S.member` keys   = pac `move` (0, -manPacSpeed)
+      | down `S.member` keys = pac `move` (0, manPacSpeed)
+      | left `S.member` keys = pac `move` (-manPacSpeed, 0)
+      | right `S.member`keys = pac `move` (manPacSpeed, 0)
+      | otherwise            = pac
+
 
 initialState :: GameState
 initialState = GameState {
